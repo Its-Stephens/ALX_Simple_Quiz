f071e8803bc33a909fd362ba18a1e51a0821c7f5
Stephen <okafors261@gmail.com>
2025-12-05 21:41:03 +0100

    quiz js task

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkAnswer(){
+    let correctAnswer = "4";
+    const userAnswer =  document.querySelector('input[name="quiz"]:checked').value;
+if(userAnswer === correctAnswer){
+    document.getElementById("feedback").textContent = "Correct! Well done";
+}
+else{
+    document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+}
+}
+const button = document.getElementById("submit-answer");
+button.addEventListener("click", checkAnswer); 
