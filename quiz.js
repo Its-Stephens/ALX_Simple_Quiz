6515d48d4eb8105d75734dd921dbf36be6cd834a
Stephen <okafors261@gmail.com>
2025-12-05 21:56:06 +0100

    update && git push -u origin main

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer(){
     let correctAnswer = "4";
     const userAnswer =  document.querySelector('input[name="quiz"]:checked').value;
 if (userAnswer === correctAnswer){
-    document.getElementById("feedback").textContent = "Correct! Well done";
+    document.getElementById("feedback").textContent = "Correct! Well done.";
 }
 else {
     document.getElementById("feedback").textContent = "That's incorrect. Try again!";
